d5ea38f63f916bc6d3df98f8e63fb28c7dcdc5dd
Valentin Guillaume <etu49080@henallux.be>
2025-01-05 15:21:41 +0100

    maj PEPPER

--- util/index.js
+++ util/index.js
@@ -1,13 +1,13 @@
 import * as argon2 from 'argon2';
 
 export const hash = (password) => {
-    return argon2.hash(password, {secret: Buffer.from(process.env.Pepper)});
+    return argon2.hash(password, {secret: Buffer.from(process.env.PEPPER)});
 };
 
 export const compare = (plainText, hash) => {
     return argon2.verify(
         hash,
         plainText,
-        {secret: Buffer.from(process.env.Pepper)}
+        {secret: Buffer.from(process.env.PEPPER)}
     );
 };
